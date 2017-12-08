25414125ef3b47c35f6aecb4fac7bc495ae2570e
nemohgz <husthgz@foxmail.com>
2017-12-08 21:41:27 +0800

    coding File read and write part

--- src/main/java/com/hgz/study/IOTest.java
+++ src/main/java/com/hgz/study/IOTest.java
@@ -9,6 +9,15 @@ package com.hgz.study;
  */
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class IOTest {
@@ -53,20 +62,85 @@ public class IOTest {
             System.out.println(e.toString());
         }*/
         //java scanner
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter a number");
-        b = in.nextInt();
-        System.out.println("Enter a string");
-        String s = in.nextLine();
-        System.out.println("you Entered: " + b + " " + s);
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter a number");
+            b = in.nextInt();
+            System.out.println("Enter a string");
+            in.nextLine();
+            String s = in.nextLine();
+            System.out.println("you Entered: " + b + " " + s);
+        } catch (NoSuchElementException e) {
+            System.out.println(e.toString());
+        }
 
         System.out.println("standard System output stream test");
     }
     private void fileIOTest() {
+        //File create
+        File fl = new File("./tmp.txt");
+        try {
+            if(!fl.exists()) {
+                fl.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return;
+        }
+        try {
+            FileOutputStream wrf = new FileOutputStream(fl);
+            String filecontent = "FileInput/OutputStream write and read test successfully";
+            wrf.write(filecontent.getBytes() ,0 ,filecontent.getBytes().length);
+
+            wrf.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
         //FileInputStream
-        //FileInputStream fis = new FileInputStream()
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(fl);
+            byte[] buf = new byte[1024];
+            while(fis.read(buf) != -1) {
+                System.out.print(new String(buf));
+            }
+            System.out.println("");
+            fis.close();
+            fl.delete();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
         //FileReader
+        try {
+            FileWriter fwr = new FileWriter(fl);
+            String filecontent = "FileReader/Writer write and read test successfully";
+            fwr.write(filecontent);
+            fwr.close();
+            FileReader frd = new FileReader(fl);
+            char[] buf = new char[1024];
+            while(frd.read(buf) != -1) {
+                System.out.print(new String(buf));
+            }
+            System.out.println("");
+            frd.close();
+            fl.delete();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
         //BufferedReader
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fl));
+            String filecontent = "BufferedReader/Writer write and read test successfully";
+            bw.write(filecontent);
+            bw.close();
+            BufferedReader br = new BufferedReader(new FileReader(fl));
+            System.out.println(br.readLine());
+            br.close();
+            fl.delete();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
     }
     private void netIOTest() {
 
