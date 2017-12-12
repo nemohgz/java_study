1e46126813ec1a0861c6c1ba0510f0b5a266ff84
nemohgz <husthgz@foxmail.com>
2017-12-12 21:28:45 +0800

    add Array and List part

--- src/main/java/com/hgz/study/ArrayTest.java
+++ src/main/java/com/hgz/study/ArrayTest.java
@@ -1,5 +1,10 @@
 package com.hgz.study;
 
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @Author: HGZ
  * @Date: 2017-12-07
@@ -8,4 +13,30 @@ package com.hgz.study;
  * @Motifiedy by:
  */
 public class ArrayTest {
+    public ArrayTest(){}
+    public void test() {
+        arrayTest();
+        arrayListTest();
+    }
+    public void arrayTest() {
+        String[]  aArray = new String[5];
+        String[]  bArray = {"a", "b", "c", "d", "e"};
+        String[]  cArray = new String[]{"1", "3", "2", "5", "4"};
+        System.arraycopy(bArray, 0, aArray, 0, aArray.length);
+        System.out.println(aArray);
+        System.out.println(Arrays.toString(aArray));
+        String[] dArray = Arrays.copyOf(cArray, cArray.length);
+        Arrays.sort(dArray);
+        System.out.println(Arrays.toString(dArray));
+    }
+    public  void arrayListTest() {
+        ArrayList<String>  alist = new ArrayList<>();
+        alist.add("a");
+        alist.add("b");
+        alist.add("c");
+        String[] bArray = alist.toArray(new String[]{});
+        for (String e: bArray) {
+            System.out.println(e);
+        }
+    }
 }
--- src/main/java/com/hgz/study/CollectionTest.java
+++ src/main/java/com/hgz/study/CollectionTest.java
@@ -1,6 +1,12 @@
 package com.hgz.study;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -20,28 +26,64 @@ public class CollectionTest {
         mapTest();
     }
     public void listTest() {
-
+        List<String> slist = new ArrayList<String>();
+        slist.add("1");
+        slist.add("2");
+        slist.add("3");
+        Iterator<String> iter = slist.iterator();
+        System.out.println("Iterator Testing");
+        while(iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+        ListIterator<String> listIter = slist.listIterator();
+        System.out.println("ListIterator  Previous Testing");
+        while(listIter.hasNext()) {
+            listIter.next();
+        }
+        while(listIter.hasPrevious()) {
+            System.out.println(listIter.previous());
+        }
+        listIter.next();
+        listIter.remove();
+        System.out.println("now remove first element");
+        while(listIter.hasNext()) {
+            System.out.println(listIter.next());
+        }
     }
     public void setTest() {
 
     }
     public void  queueTest() {
+        //
         ArrayBlockingQueue<String> abq = new ArrayBlockingQueue<String>(100);
         abq.add("123");
         abq.offer("234");
         abq.offer("345");
+        System.out.println("ArrayBlockingQueue Testing");
         System.out.println(Arrays.toString(abq.toArray()));
         System.out.println(abq.poll());
         System.out.println(abq.remove());
         System.out.println(abq.poll());
+        //
         LinkedBlockingQueue<String> lbq = new LinkedBlockingQueue<String>(100);
         lbq.add("abc");
         lbq.offer("def");
         lbq.offer("ghi");
+        System.out.println("LinkedBlockingQueue Testing");
         System.out.println(Arrays.toString(lbq.toArray()));
         System.out.println(lbq.poll());
         System.out.println(lbq.remove());
         System.out.println(lbq.poll());
+        //
+        PriorityQueue<String> pq = new PriorityQueue<>();
+        pq.add("132");
+        pq.add("123");
+        pq.add("231");
+        pq.add("213");
+        System.out.println("PriorityQueue Testing");
+        while ( pq.peek() != null) {
+            System.out.println(pq.poll());
+        }
     }
     public void mapTest() {
 
