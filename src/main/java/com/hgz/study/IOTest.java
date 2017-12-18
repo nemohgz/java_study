c0211a32fbcc8c936e8de1a8a1ff17347509faee
nemohgz <husthgz@foxmail.com>
2017-12-18 16:06:53 +0800

    cadd sckocket io for IOTest

--- src/main/java/com/hgz/study/IOTest.java
+++ src/main/java/com/hgz/study/IOTest.java
@@ -9,19 +9,27 @@ package com.hgz.study;
  */
 
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class IOTest {
     private int b;
+    private static final int PORT = 10086;
     public IOTest() {
         b = 0;
     }
@@ -143,9 +151,105 @@ public class IOTest {
         }
     }
     private void netIOTest() {
-
+          if (EventTest.operateSystemSupported()) {
+              ClientTest clientTest = new ClientTest(this.PORT);
+              new Thread(clientTest).start();
+          } else {
+              ServerTest serverTest = new ServerTest(this.PORT);
+              new Thread(serverTest).start();
+          }
     }
+    class ServerTest implements  Runnable{
+        private ServerSocket serverSocket;
 
+        public ServerTest(int port) {
+            try {
+                this.serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                System.out.println("cannot start server of port:" + PORT);
+                e.printStackTrace();
+            }
+        }
+        public void run() {
+            startServer();
+        }
+        private void startServer() {
+            if (serverSocket == null) return;
+            Socket client = null;
+            try {
+                client = serverSocket.accept();
+                while (true) {
+                    DataInputStream in = new DataInputStream(client.getInputStream());
+                    StringBuffer sb = new StringBuffer();
+                    sb.append("I am server ");
+                    sb.append("reply your message -->");
+                    String clientString = in.readUTF();
+                    System.out.println("server:client say " + clientString);
+                    sb.append(clientString);
+                    DataOutputStream out = new DataOutputStream(client.getOutputStream());
+                    out.writeUTF(sb.toString());
+                    in.close();
+                    out.close();
+                }
+            } catch (IOException e) {
+                System.out.println("server closed for exception: " + e.getMessage());
+            } finally {
+                if (client != null) {
+                    try {
+                        client.close();
+                        client = null;
+                    } catch (IOException e) {
+
+                    }
+                }
+                if (serverSocket != null) {
+                    try {
+                        serverSocket.close();
+                        serverSocket = null;
+                    } catch (IOException e) {
+
+                    }
+                }
+            }
+        }
+    }
+    class ClientTest implements  Runnable{
+        private static final String SERVER_IP = "47.100.110.207";
+        private Socket clientSocket;
+
+        public ClientTest(int port) {
+            try {
+                clientSocket = new Socket(SERVER_IP, port);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("cannot create client socket");
+            }
+        }
+        public  void run() {
+            connectServer();
+        }
+        private void connectServer() {
+            try {
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                out.writeUTF("Hi I am a client");
+                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+                System.out.println("client: server say " + in.readUTF());
+                out.close();
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (clientSocket != null) {
+                    try {
+                        clientSocket.close();
+                        clientSocket = null;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         IOTest ioTest = new IOTest();
         ioTest.test();
